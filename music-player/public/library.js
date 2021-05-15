72bd4b30fb0244492b5eb9d36d494acaea9b9700
imnotgivingup <praneethb2002@gmail.com>
2021-05-16 03:17:07 +0530

    playlists page looks bit better

--- music-player/public/library.js
+++ music-player/public/library.js
@@ -1,101 +1,154 @@
 var newplsongs = [];
 var newplids = [];
+var result = null;
 
 
 function showpl() {
     axios.get('/getPlaylists', { withCredentials: true }).then((response) => {
+        result = response.data;
         var plshtml = "";
-        console.log('[' + response.data[0].songnames.toString() + ']');
         for (var i in response.data) {
-            plshtml += `<div class="col-sm-3" style="padding-top:1%">
-            <div class="card">
-            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light"data-mdb-toggle="modal"
-            data-mdb-target="#plmodal" onclick="showplsongs('`+ (response.data[i].songnames).toString() + `')">
-            <img src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" class="card-img-top" alt="..." />
-            <a>
-              <div class="mask d-flex justify-content-center align-items-center"
+            if (i < 2) {
+                var pls = document.getElementById("pls0");
+                plshtml += `<div class="col-sm-2" style="padding-top:1%">
+                <div class="card">
+                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light"
+                onclick="showplsongs('`+ (response.data[i]._id).toString() + `')">
+                <img src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" class="card-img-top" alt="..." />
+                <a>
+                <div class="mask d-flex justify-content-center align-items-center"
                 style="background-color: rgba(0, 0, 0, 0.45);">
                 <button class="btn btn-primary btn-floating bg-white">
-                  <i class="fa fa-play-circle fa-3x" style="color:#0f64f2"></i>
+                <i class="fa fa-play-circle fa-3x" style="color:#0f64f2"></i>
                 </button>
-              </div>
-            </a>
-          </div>
+                </div>
+                </a>
+                </div>
                 <div class="card-body">
-                  <h5 class="card-title">`+ response.data[i].playlistname + `</h5>
+                <h5 class="card-title">`+ response.data[i].playlistname + `</h5>
                 </div></div></div>`;
+                pls.innerHTML += plshtml;
+                plshtml = "";
+            }
+            else {
+                if (i % 3 == 2) {
+                    plshtml += `<div class="carousel-item"> <div class="row"">`;
+                    plshtml += `<div class="col-sm-3" style="padding-top: 1%"></div>`;
+                }
 
+                plshtml += `<div class="col-sm-2" style="padding-top:1%">
+                <div class="card">
+                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light"
+                onclick="showplsongs('`+ (response.data[i]._id).toString()+ `')">
+                <img src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" class="card-img-top" alt="..." />
+                <a>
+                <div class="mask d-flex justify-content-center align-items-center"
+                style="background-color: rgba(0, 0, 0, 0.45);">
+                <button class="btn btn-primary btn-floating bg-white">
+                <i class="fa fa-play-circle fa-3x" style="color:#0f64f2"></i>
+                </button>
+                </div>
+                </a>
+                </div>
+                <div class="card-body">
+                <h5 class="card-title">`+ response.data[i].playlistname + `</h5>
+                </div></div></div>`;
+                if (i % 3 == 1) {
+                    plshtml += `</div></div>`
+                    document.getElementById("car").innerHTML += plshtml;
+                    plshtml = ""
+                }
+            }
         }
 
-        document.getElementById("pls").innerHTML += plshtml;
+
+
     });
 }
 
-var gotSongs = 0;
-var songs = null;
-function getSongs() {
-    if (!gotSongs) {
-        axios.get('/getSongs').then((response) => {
-            gotSongs = 1;
-            songs = response.data;
-        });
+// var gotSongs = 0;
+// var songs = null;
+// function getSongs() {
+//     if (!gotSongs) {
+//         axios.get('/getSongs').then((response) => {
+//             gotSongs = 1;
+//             songs = response.data;
+//         });
 
-    }
-}
+//     }
+// }
 
-function dynamic_search(event) {
-    if (gotSongs) {
-        var x = event.keyCode;
-        var sugg = document.getElementById("sugg");
-        if (x >= 32 || x === 8) {
-            var squery = document.getElementById("songname");
-            var search_text = squery.value;
-            var slength = search_text.length;
-            if (!slength) {
-                sugg.innerHTML = ""
-                return null;
-            }
-            search_text = search_text.toLowerCase();
-            var to_display = [];
-            for (var i = 0; i < songs.length; i++) {
-                if (songs[i].filename.slice(0, slength) == search_text) {
-                    to_display.push({ name: songs[i].filename, id: songs[i]._id });
+// function newplaylist(u, d) {
+//     axios.post('/newPlaylist', {
+//         playlistname: document.getElementById('plname').value,
+//         songnames: newplsongs,
+//         songids: newplids,
+//         update: u,
+//         delete: d
+//     }, { withCredentials: true })
+//         .then(function (response) {
+//         })
+//     newplsongs = [];
+//     newplids = [];
+// }
+
+
+// function addtolist(sname, id) {
+//     newplsongs.push(sname);
+//     newplids.push(id);
+// }
+
+function showplsongs(plid) {
+    var inner="";
+    var flag=1;
+    document.getElementById("carousel2").remove();
+    for(var i in result){
+        if(result[i]._id==plid){
+            console.log(result[i]);
+            inner += `<div id="carousel2"><h4 style="color: white">Songs in ` + result[i].playlistname +`</h4><div id="carouselExampleControls2" class="carousel slide" data-mdb-ride="carousel"><div class="carousel-inner" id="car2">`;
+            for(var j=0;j<result[i].songnames.length;j++){
+                if(j%3==0){
+                    var x="";
+                    if (j==0){x="active";}
+                    inner +=`<div class="carousel-item `+x+` "><div class="row"><div class="col-sm-3" style="padding-top: 1%"></div>`;
+                    flag=1;
+                }
+                inner += `<div class="col-sm-2" style="padding-top:1%">
+                <div class="card">
+                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light"
+                onclick="">
+                <img src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" class="card-img-top" alt="..." />
+                <a>
+                <div class="mask d-flex justify-content-center align-items-center"
+                style="background-color: rgba(0, 0, 0, 0.45);">
+                <button class="btn btn-primary btn-floating bg-white">
+                <i class="fa fa-play-circle fa-3x" style="color:#0f64f2"></i>
+                </button>
+                </div>
+                </a>
+                </div>
+                <div class="card-body">
+                <h5 class="card-title">`+ result[i].songnames[j] + `</h5>
+                </div></div></div>`;
+                if(j%3==2){
+                    inner +=`</div></div>`;
+                    flag=0;
                 }
             }
-            sugg.innerHTML = ""
-            for (var i = 0; i < to_display.length; i++) {
-                sugg.innerHTML = sugg.innerHTML + "<br>" + `<button type="button" class="btn btn-floating" onclick = "addtolist('` + to_display[i].name + `,` + to_display[i].id+`')"><i class="fa fa-plus-circle fa-3x" style="color:#0f64f2"></i></button>` + to_display[i].name;
-            }
+            if (flag) { inner += `</div></div>`;}
+            inner +=`</div>
+            <button class="carousel-control-prev"  type="button"  data-mdb-target="#carouselExampleControls2"  data-mdb-slide="prev" >
+            <span  class="carousel-control-prev-icon"   aria-hidden="true" ></span>
+            <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-mdb-target="#carouselExampleControls2"   data-mdb-slide="next"   >
+            <span   class="carousel-control-next-icon"  aria-hidden="true"  ></span>
+            <span class="visually-hidden">Next</span>
+            </button>
+            </div>
+            </div>`;
+            document.getElementById("container").innerHTML+=inner;
+            break;
         }
-    }
-}
-
-function newplaylist(u,d) {
-    axios.post('/newPlaylist', {
-        playlistname: document.getElementById('plname').value,
-        songnames: newplsongs,
-        songids: newplids,
-        update: u,
-        delete: d
-    }, { withCredentials: true })
-        .then(function (response) {
-            //   console.log(response);
-        })
-    newplsongs = [];
-    newplids = [];
-}
-
-
-function addtolist(sname,id) {
-    newplsongs.push(sname);
-    newplids.push(id);
-}
-
-function showplsongs(songliststr) {
-    var songlist = songliststr.split(",");
-    console.log(songlist);
-    var plmodalbody = document.getElementById("plmodalbody");
-    for (var i in songlist) {
-        plmodalbody.innerHTML += `<ul><li>` + songlist[i] + `</li></ul>`;
     }
 }
