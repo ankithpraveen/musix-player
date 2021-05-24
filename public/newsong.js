ca9730e88aa4a2699b0cf062a26d287e158576ad
Ankith Praveen <kith.304@gmail.com>
2021-05-24 19:54:53 +0530

    file ext check

--- public/newsong.js
+++ public/newsong.js
@@ -2,7 +2,7 @@ function upload() {
     var formData = new FormData();
     var song = document.querySelector('#file1');
     var extension = (song.files[0].name).substring((song.files[0].name).lastIndexOf('.')+1);
-    if (extension == "mp3" || extension == "wav"){
+    if (extension == "mp3" || extension == "wav" || extension == "m4a"){
         if (song.files[0].size / 1048576 < 10) {
             formData.append("song_name", document.getElementById('s_name').value);
             formData.append("artist_name", document.getElementById('a_name').value);
