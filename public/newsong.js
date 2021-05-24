c845c07e56511bbc1b33e812e41c249324a8eb99
Ankith Praveen <kith.304@gmail.com>
2021-05-24 19:21:13 +0530

    file ext check

--- public/newsong.js
+++ public/newsong.js
@@ -1,28 +1,34 @@
 function upload() {
     var formData = new FormData();
     var song = document.querySelector('#file1');
-    if (song.files[0].size / 1048576 < 10) {
-        formData.append("song_name", document.getElementById('s_name').value);
-        formData.append("artist_name", document.getElementById('a_name').value);
-        formData.append("song", song.files[0]);
-        console.log(formData);
-        axios.post('/uploadSong', formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-                withCredentials: true
-            }
-        })
-            .then((response) => {
-                alert('Upload success boss');
-                console.log(response);
-            }, (error) => {
-                console.log(error);
-            });
+    var extension = (song.files[0].name).substring((song.files[0].name).lastIndexOf('.')+1);
+    if (extension == "mp3" || extension == "wav"){
+        if (song.files[0].size / 1048576 < 10) {
+            formData.append("song_name", document.getElementById('s_name').value);
+            formData.append("artist_name", document.getElementById('a_name').value);
+            formData.append("song", song.files[0]);
+            console.log(formData);
+            axios.post('/uploadSong', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    withCredentials: true
+                }
+            })
+                .then((response) => {
+                    alert('Upload success boss');
+                    console.log(response);
+                }, (error) => {
+                    console.log(error);
+                });
+            
         
-    
+        }
+        else{
+            alert("too big");
+            
+        }
     }
     else{
-        alert("too big");
-        
+        alert("Only mp3 or wav files are supported");
     }
 }
